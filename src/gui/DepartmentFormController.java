629910e55c3413bb3d992828b4880fc32a2812c8
Amanda Maschio <maschioamanda@hotmail.com>
2021-05-04 18:04:45 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -78,6 +81,9 @@ public class DepartmentFormController implements Initializable {
 		catch(DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
+		catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
 	}
 	
 	/**
@@ -102,8 +108,23 @@ public class DepartmentFormController implements Initializable {
 	private Department getFormData() {
 		
 		Department obj = new Department();
+		
+		//Instanciando a exceção personalizada
+		ValidationException exception = new ValidationException("Validation error");
+		
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		//Testar se o campo Name está vazio
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("Name", "Field can't be empty");
+		}
+
 		obj.setName(txtName.getText());
+		
+		//Se o Map de erros tiver algum conteúdo jogamos a exceção
+		if(exception.getErrors().size() > 0) {
+			throw exception;
+		}
 		
 		return obj;
 	}
@@ -137,6 +158,19 @@ public class DepartmentFormController implements Initializable {
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
 		
+	}
+	
+	/**
+	 * Método responsável por capturar os erros armazenados no Map de erros do ValidationException e exibir as mensagens no lbl correspondente
+	 * @param error
+	 */
+	private void setErrorMessages(Map<String,String> errors) {
+		
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("Name")) {
+			labelErrorName.setText(errors.get("Name"));
+		}
 	}
 	
 }
