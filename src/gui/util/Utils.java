afd5f66bb248f28c1b970d7c6749732e9c659cd5
Amanda Maschio <maschioamanda@hotmail.com>
2021-05-12 17:14:16 -0300

    Seller TableView

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,7 +1,13 @@
 package gui.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 public class Utils {
@@ -29,5 +35,57 @@ public class Utils {
 		catch(NumberFormatException e) {
 			return null;
 		}
+	}
+	
+	/**
+	 * Método responsável por formatar um Date em um SimpleDateFormat de uma Table
+	 * @param <T>
+	 * @param tableColumn
+	 * @param format
+	 */
+	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
+	
+		tableColumn.setCellFactory(column -> {
+			
+			TableCell<T, Date> cell = new TableCell<T, Date>() {
+				private SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+				@Override
+				protected void updateItem(Date item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						setText(sdf.format(item));
+					}
+				}
+			};
+			
+			return cell;
+		});
+	}
+	
+	/**
+	 * Método responsável por formatar um Double de uma Table
+	 * @param <T>
+	 * @param tableColumn
+	 * @param decimalPlaces
+	 */
+	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Double> cell = new TableCell<T, Double>() {
+				@Override
+				protected void updateItem(Double item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						Locale.setDefault(Locale.US);
+						setText(String.format("%." + decimalPlaces + "f", item));
+					}
+				}
+			};
+			return cell;
+		});
 	}
 }
