33e9ec7b29b0f7a98f53dd145e7aaa9b467c1cdb
Amanda Maschio <maschioamanda@hotmail.com>
2021-05-12 17:30:04 -0300

    SellerForm

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -127,40 +132,40 @@ public class SellerListController implements Initializable, DataChangeListener {
 	}
 	
 	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
-//		
-//		try {
-//			
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-//			Pane pane = loader.load();
-//			
-//			//Pegar referência para o controlador SellerFormController
-//			SellerFormController controller = loader.getController();
-//			
-//			//Injetar o Seller obj no controller do SellerFormController
-//			controller.setSeller(obj);
-//			
-//			//Injetar o SellerService
-//			controller.setSellerService(new SellerService());
-//			
-//			controller.subscribeDataChangeListener(this);
-//			
-//			//Carrega os dados do obj no formulario
-//			controller.updateFormData();
-//						
-//			//Para janelas modal devemos instanciar um novo Stage (um palco em cima do outro)
-//			Stage dialogStage = new Stage();
-//			
-//			//Configurando o novo Stage
-//			dialogStage.setTitle("Enter Seller data");
-//			dialogStage.setScene(new Scene(pane));
-//			dialogStage.setResizable(false);
-//			dialogStage.initOwner(parentStage);
-//			dialogStage.initModality(Modality.WINDOW_MODAL);
-//			dialogStage.showAndWait();
-//		}
-//		catch(IOException e) {
-//			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-//		}
+		
+		try {
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			//Pegar referência para o controlador SellerFormController
+			SellerFormController controller = loader.getController();
+			
+			//Injetar o Seller obj no controller do SellerFormController
+			controller.setSeller(obj);
+			
+			//Injetar o SellerService
+			controller.setSellerService(new SellerService());
+			
+			controller.subscribeDataChangeListener(this);
+			
+			//Carrega os dados do obj no formulario
+			controller.updateFormData();
+						
+			//Para janelas modal devemos instanciar um novo Stage (um palco em cima do outro)
+			Stage dialogStage = new Stage();
+			
+			//Configurando o novo Stage
+			dialogStage.setTitle("Enter Seller data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@Override
